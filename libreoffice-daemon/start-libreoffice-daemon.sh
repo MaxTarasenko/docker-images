a8d438273f305910ee2757d40cdab026f9c5573b
Maksym Tarasenko <maksym.tarasenko@chisw.com>
2024-01-23 13:27:06 +0200

    libreoffice 0.3

--- libreoffice-daemon/start-libreoffice-daemon.sh
+++ libreoffice-daemon/start-libreoffice-daemon.sh
@@ -4,4 +4,4 @@
 echo "Running LibreOffice on port 8100"
 
 # Running LibreOffice in command listening mode
-soffice --invisible --nologo --norestore --headless --nofirststartwizard --accept="socket,host=0.0.0.0,port=8100;urp;" > /var/log/libreoffice.log 2>&1
+soffice --backtrace --sync --invisible --nologo --norestore --headless --nofirststartwizard --accept="socket,host=0.0.0.0,port=8100;urp;"
